3390f9c2ff031cac30bd9475858dfa513bd12c13
Daniel Henrique <henriqueevaldo@outlook.com>
2022-09-29 08:38:10 -0300

    model creating process 👷

--- api/models.py
+++ api/models.py
@@ -1,8 +1,10 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
-
+import uuid
 # Create your models here.
 
 class Address(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     street = models.CharField(max_length=255, null=False, blank=False)
     neighborhood = models.CharField(max_length=50, null=False, blank=False)
     city = models.Model(max_length=50, null=False, blank=False)
@@ -10,7 +12,20 @@ class Address(models.Model):
     zipcode = models.CharField(max_length=9, null=False, blank=False)
     complement = models.CharField(max_length=50, null=False, blank=False)
 
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    cpf = models.CharField(max_length=14)
+    password = models.CharField()
+
 class Client(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     cpf = models.CharField(max_length=14)
     address = models.ForeignKey()
+
+class Account(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    client_referer
+    balance
+    account_number = models.IntegerField()
+    agency
