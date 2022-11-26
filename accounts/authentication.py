26e31754c1c996a192e5ac82d8b1941d52f1dcfe
Daniel Henrique <henriqueevaldo@outlook.com>
2022-11-26 12:54:05 -0300

    feat(auth): create refresh token

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -16,3 +16,10 @@ def decode_access_token(token):
         return payload['user_id']
     except:
         raise exceptions.AuthenticationFailed('unauthenticated')
+
+def create_refresh_token(id):
+    return jwt.encode({
+        'user_id': id,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(days=7),
+        'iat': datetime.datetime.utcnow()
+    }, 'refresh_secret', algorithm='HS256')
