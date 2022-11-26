453105ec10c3d361ea76fcfdb9bd67164c4d7fce
Daniel Henrique <henriqueevaldo@outlook.com>
2022-11-26 12:53:18 -0300

    feat(auth): create access token

---
+++ accounts/authentication.py
@@ -0,0 +1,10 @@
+import jwt, datetime
+from rest_framework import exceptions
+
+
+def create_access_token(id):
+    return jwt.encode({
+        'user_id': id,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=30),
+        'iat': datetime.datetime.utcnow()
+    }, 'access_secret', algorithm='HS256')
